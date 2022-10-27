860dc3460c791fc031b9a7f88d25534173a4c1ee
Ademiju <hardaymyju@gmail.com>
2022-10-27 14:41:46 +0100

    make it better

--- src/main/java/com/hnginternship/apiEndpoint/services/UserServiceImpl.java
+++ src/main/java/com/hnginternship/apiEndpoint/services/UserServiceImpl.java
@@ -8,7 +8,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserDetails() {
         User user = new User("Ademiju1",true,27,
-                "I'm a Java Backend Engineer.\nLooking to collaborate and hone my skills from beginner to mid-level");
+                "I'm a Java Backend Engineer, looking to collaborate and hone my skills from beginner to mid-level");
         return user;
     }
 }
