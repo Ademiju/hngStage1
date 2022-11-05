8795ac0b5dba56cbf6ca502f9ef0f6e7fe38d511
Ademiju <hardaymyju@gmail.com>
2022-11-05 09:36:45 +0100

    task 2 response adjusted

--- src/main/java/com/hnginternship/apiEndpoint/dtos/responses/ArithmeticResponse.java
+++ src/main/java/com/hnginternship/apiEndpoint/dtos/responses/ArithmeticResponse.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ArithmeticResponse {
     private String slackUsername;
-    private OperationType operationType;
+    private OperationType operation_type;
     private int result;
 
 }
